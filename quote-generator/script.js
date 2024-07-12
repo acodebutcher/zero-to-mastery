b5400afbe82077728cb73bbde2f06cf7bdd45c18
Adji <youAdji@example.com>
2024-07-12 13:58:57 +0200

    Added JS: tweetBtn, newQuoteBtn and loader

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -1,17 +1,56 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText= document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
 // let - changeable value (variable)
 // conts - not changeable constant
 let apiQuotes = [];
 
+// Show loading
+function loading () {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide loading
+function complete() {
+    
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+
+}
+
 // Show new quote
 function newQuote() {
+    loading();
     // Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
+
+    // Check if author field is blank/null and replace it with 'unknown'
+    if(!quote.author) {
+        authorText.textContent = 'Unknown';
+    } else {
+        authorText.textContent = quote.author;
+    }
+
+    // Check Quote length to determin styling
+    if (quote.text.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+
+    // Set quote & hide loader
+    quoteText.textContent = quote.text;
+    complete();
 }
 
 // Get Quotes from API
-// https://jacintodesign.github.io/quotes-api/data/quotes.json
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
     try {
         const response = await fetch(apiUrl);
@@ -23,6 +62,18 @@ async function getQuotes() {
         console.log(error);
     }
 }
+
+// Tweet Quote
+// `` enables to option to pass a variable to that string: ${}
+// '' only counts as normal (static) string
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank')
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote)
+twitterBtn.addEventListener('click', tweetQuote);
 
 // On load
 getQuotes();
