3dace8bf681b7b4f80709c69c844b06047c52138
Adji <youAdji@example.com>
2024-07-13 13:08:30 +0200

    JS Refactoring

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -5,27 +5,21 @@ const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
 const loader = document.getElementById('loader');
 
-// let - changeable value (variable)
-// conts - not changeable constant
 let apiQuotes = [];
 
-// Show loading
-function loading () {
+function showLoadingSpinner () {
     loader.hidden = false;
     quoteContainer.hidden = true;
 }
 
-// Hide loading
-function complete() {
-    
+function hideLoadingSpinner() {
     quoteContainer.hidden = false;
     loader.hidden = true;
-
 }
 
-// Show new quote
-function newQuote() {
-    loading();
+function showNewQuote() {
+    showLoadingSpinner();
+
     // Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
@@ -43,37 +37,31 @@ function newQuote() {
         quoteText.classList.remove('long-quote');
     }
 
-    // Set quote & hide loader
     quoteText.textContent = quote.text;
-    complete();
+    hideLoadingSpinner();
 }
 
 // Get Quotes from API
-async function getQuotes() {
-    loading();
+async function getQuotesFromApi() {
+    showLoadingSpinner();
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
     try {
         const response = await fetch(apiUrl);
         apiQuotes = await response.json();
-        // console.log(apiQuotes);
-        newQuote();
+        showNewQuote();
     } catch (error) {
-        // Catching error here
         console.log(error);
     }
 }
 
-// Tweet Quote
-// `` enables to option to pass a variable to that string: ${}
-// '' only counts as normal (static) string
 function tweetQuote() {
     const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
     window.open(twitterUrl, '_blank')
 }
 
-// Event Listeners
-newQuoteBtn.addEventListener('click', newQuote)
+// Adding Event Listeners to buttons to generate a new quote or tweet it
+newQuoteBtn.addEventListener('click', showNewQuote)
 twitterBtn.addEventListener('click', tweetQuote);
 
 // On load
-getQuotes();
+getQuotesFromApi();
